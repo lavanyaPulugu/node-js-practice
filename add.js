c380ab9db20ac07bec71c03d9c57e0286f907e96
lavanyapulugu <lavanya415@gmail.com>
2024-01-18 18:17:05 +0530

    node js practice

---
+++ add.js
@@ -0,0 +1,5 @@
+const mul = (a, b) => {
+  return a * b;
+};
+const sum = mul(5, 6);
+console.log(sum);
---
+++ index.js
@@ -0,0 +1,4 @@
+const superhero = require("./superhero");
+console.log(superhero.getName());
+superhero.setName("superman");
+console.log(superhero.getName());
